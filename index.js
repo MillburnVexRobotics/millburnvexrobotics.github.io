2def595c347e6e4ad6b6f283bab472fa1bc33893
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-12-30 16:32:18 -0500

    1% chance that all photos are of Abhinav

--- index.js
+++ index.js
@@ -7,4 +7,13 @@ $(document).ready(function() {
 		quote += "He is the most interesting man in the world";
 		unfinishedDescriptions[i].innerHTML = quote;
 	});
+
+	// there is a 1% chance that all photos become photos of Abhinav Madahar
+	if (Math.random() < 0.01) {
+		var images = $("img");
+		images.each(function(i) {
+			images[i].src = "members/hipster.jpg";
+			console.log(images[i]);
+		})
+	}
 })
